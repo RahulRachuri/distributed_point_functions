9be6217683f227141687017cc78d0051f47e3bf0
Lennart Braun <lenerd@posteo.de>
2022-07-01 14:12:16 +0200

    start mpdpf implementation

--- dpf/multi_point_distributed_point_function.cc
+++ dpf/multi_point_distributed_point_function.cc
@@ -1,1 +1,29 @@
-#include "dpf/multi_point_distributed_point_function.h"
+#include <absl/memory/memory.h>
+
+#include "multi_point_distributed_point_function.h"
+#include "status_macros.h"
+
+namespace distributed_point_functions {
+
+MultiPointDistributedPointFunction::MultiPointDistributedPointFunction(
+    const MpDpfParameters& parameters,
+    std::unique_ptr<Cuckoo>&& cuckoo_context_)
+    : parameters_(parameters), cuckoo_context_(std::move(cuckoo_context_)) {}
+
+absl::StatusOr<std::unique_ptr<MultiPointDistributedPointFunction>>
+MultiPointDistributedPointFunction::Create(const MpDpfParameters& parameters) {
+  uint64_t number_inputs = parameters.number_points();
+  DPF_ASSIGN_OR_RETURN(auto cuckoo_parameters, Cuckoo::Sample(number_inputs));
+  DPF_ASSIGN_OR_RETURN(auto cuckoo_context,
+                       Cuckoo::CreateFromParameters(cuckoo_parameters));
+  return absl::WrapUnique(new MultiPointDistributedPointFunction(
+      parameters, std::move(cuckoo_context)));
+}
+
+absl::StatusOr<std::pair<DpfKey, DpfKey>>
+MultiPointDistributedPointFunction::GenerateKeys(
+    absl::Span<absl::uint128> alphas, absl::Span<const Value> betas) {
+  // TODO
+}
+
+}  // namespace distributed_point_functions
--- dpf/multi_point_distributed_point_function.h
+++ dpf/multi_point_distributed_point_function.h
@@ -1,9 +1,11 @@
 #ifndef DISTRIBUTED_POINT_FUNCTIONS_DPF_MULTI_POINT_DISTRIBUTED_POINT_FUNCTION_H_
 #define DISTRIBUTED_POINT_FUNCTIONS_DPF_MULTI_POINT_DISTRIBUTED_POINT_FUNCTION_H_
 
-#include <absl/status/statusor.h>
+#include "absl/status/statusor.h"
+#include "dpf/cuckoo.h"
 #include "dpf/distributed_point_function.h"
 #include "dpf/distributed_point_function.pb.h"
+#include <memory>
 
 namespace distributed_point_functions {
 
@@ -27,7 +29,10 @@ class MultiPointDistributedPointFunction {
   }
 
  private:
+  MultiPointDistributedPointFunction(const MpDpfParameters& parameters, std::unique_ptr<Cuckoo>&& cuckoo_context_);
+
   const MpDpfParameters parameters_;
+  std::unique_ptr<Cuckoo> cuckoo_context_;
   std::vector<DistributedPointFunction> single_point_dpfs_;
 };
 
