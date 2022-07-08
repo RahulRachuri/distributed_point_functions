d6cc079b5314797693b22e2e146238c701e8734a
Lennart Braun <lenerd@posteo.de>
2022-07-08 17:40:30 +0200

    update mpdpf stuff

--- dpf/multi_point_distributed_point_function.cc
+++ dpf/multi_point_distributed_point_function.cc
@@ -26,22 +26,22 @@ absl::StatusOr<std::pair<DpfKey, DpfKey>>
 MultiPointDistributedPointFunction::GenerateKeys(
     absl::Span<absl::uint128> alphas, absl::Span<const Value> betas) {
   // Check validity of alphas
-  if (alphas.size() != parameters_.size()) {
+  if (alphas.size() != parameters_.number_points()) {
     return absl::InvalidArgumentError(
         "`alphas` has to have the same size as `parameters` passed at "
         "construction");
   }
-  int log_domain_size = parameters_.log_domain_size();
-  if (log_domain_size < 128)
-      for (auto alpha_i : alphas) {
-          if (alpha_i >= (absl::uint128{1} << log_domain_size)) {
-            return absl::InvalidArgumentError(
-                "each `alpha` must be smaller than the output domain size");
-          }
+  int log_domain_size = parameters_.dpf_parameters().log_domain_size();
+  if (log_domain_size < 128) {
+    for (auto alpha_i : alphas) {
+      if (alpha_i >= (absl::uint128{1} << log_domain_size)) {
+        return absl::InvalidArgumentError(
+            "each `alpha` must be smaller than the output domain size");
       }
+    }
   }
   // Check validity of betas
-  if (betas.size() != parameters_.size()) {
+  if (betas.size() != parameters_.number_points()) {
     return absl::InvalidArgumentError(
         "`betas` has to have the same size as `parameters` passed at "
         "construction");
@@ -55,11 +55,13 @@ MultiPointDistributedPointFunction::GenerateKeys(
   // TODO
 
   DPF_ASSIGN_OR_RETURN(auto cuckoo_table, cuckoo_context_->HashCuckoo(alphas));
-  const auto& [cuckoo_table_items, cuckoo_table_indices, cuckoo_table_occupied] = cuckoo_table;
+  const auto& [cuckoo_table_items, cuckoo_table_indices,
+               cuckoo_table_occupied] = cuckoo_table;
   DPF_ASSIGN_OR_RETURN(auto simple_htable, cuckoo_context_->HashSimple(alphas));
 
-  auto pos = [&simple_htable](auto bucket_i, auto item){
-    auto iter = std::find(std::begin(simple_htable[bucket_i]), std::end(simple_htable[bucket_i]), item);
+  auto pos = [&simple_htable](auto bucket_i, auto item) {
+    auto iter = std::find(std::begin(simple_htable[bucket_i]),
+                          std::end(simple_htable[bucket_i]), item);
 
     return std::distance(std::begin(simple_htable[bucket_i]), iter);
   };
@@ -69,7 +71,7 @@ MultiPointDistributedPointFunction::GenerateKeys(
   // std::vector<std::unique_ptr<DistributedPointFunction>> sp_dpfs;
   // sp_dpfs.reserve(num_buckets);
 
-  const auto value_type = parameters_.dpf_parameters().value_type();  
+  const auto value_type = parameters_.dpf_parameters().value_type();
 
   std::vector<DpfParameters> sp_dpf_params(num_buckets);
 
@@ -82,26 +84,29 @@ MultiPointDistributedPointFunction::GenerateKeys(
   for (uint64_t bucket_i = 0; bucket_i < num_buckets; ++bucket_i) {
     *sp_dpf_params[bucket_i].mutable_value_type() = value_type;
 
-    sp_dpf_params[bucket_i].set_log_domain_size(static_cast<int32_t>(std::ceil(std::log2(simple_htable[bucket_i].size()))));
+    sp_dpf_params[bucket_i].set_log_domain_size(static_cast<int32_t>(
+        std::ceil(std::log2(simple_htable[bucket_i].size()))));
 
-    DPF_ASSIGN_OR_RETURN(auto sp_dpf, DistributedPointFunction::Create(sp_dpf_params[bucket_i]) );
+    DPF_ASSIGN_OR_RETURN(
+        auto sp_dpf, DistributedPointFunction::Create(sp_dpf_params[bucket_i]));
 
     // sp_dpfs.push_back(std::move(sp_dpf));
 
     absl::uint128 a;
     Value b;
 
-    if(cuckoo_table_occupied[bucket_i]) {
+    if (cuckoo_table_occupied[bucket_i]) {
       a = pos(bucket_i, cuckoo_table_items[bucket_i]);
       b = betas[cuckoo_table_indices[bucket_i]];
+    } else {
+      a = 0;
+      DPF_ASSIGN_OR_RETURN(b, dpf_internal::MakeZero(value_type));
     }
-    else {
 
-    }
-
-    DPF_ASSIGN_OR_RETURN(auto keys, sp_dpf.GenerateKeys() )
+    DPF_ASSIGN_OR_RETURN(auto keys, sp_dpf->GenerateKeys(a, b));
+    keys_1.push_back(std::move(keys.first));
+    keys_2.push_back(std::move(keys.second));
   }
-
 }
 
 }  // namespace distributed_point_functions
